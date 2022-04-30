4f5f6dfb69c348f6d9b30dd0634a2c0af7c19442
emnigor <mumijtrol25@gmail.com>
2022-04-30 14:35:50 +0300

    feat: encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let count=1;
+  let string=''
+  for(i=0; i<str.length; i++){
+    if (str[i]===str[i+1]){
+      count++
+    }
+    if (str[i]!==str[i+1] && count>1){
+      
+      string=string+count+str[i]
+      count=1
+    }
+    if (str[i]!==str[i+1] && str[i]!==str[i-1] && count===1){
+      string=string+str[i]
+    }
+  }
+  return string
 }
 
 module.exports = {
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -12,11 +12,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * depthCalc.calculateDepth([[[]]]) => 3
  *
  */
+
+
+//  class User {
+
+//   constructor(name) {
+//     this.name = name;
+//   }
+
+//   sayHi() {
+//     alert(this.name);
+//   }
+
+// }
+
+// // Использование:
+// let user = new User("Иван");
+// user.sayHi();
+
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(arr){
+    this.arr=arr
   }
+  calculateDepth( arr ) {
+   let result=1
+    arr.forEach(element => {
+      if(Array.isArray(element)) {
+        result++
+        calculateDepth(element )
+      }
+    });
+    return result
+  }
+  
 }
 
 module.exports = {
