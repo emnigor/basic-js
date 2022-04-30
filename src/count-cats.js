37f7ad9404ef4a3db79e0bc0470da151216ecff9
emnigor <mumijtrol25@gmail.com>
2022-04-30 11:58:38 +0300

    feat: result cats and arr

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,10 +14,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  // throw new NotImplementedError('Not implemented');
+  // console.log(matrix)
+  let counter=0
+  for (i=0; i<matrix.length; i++) {
+    for (j=0; j<matrix[i].length; j++) {
+    if (matrix[i][j]==='^^') {
+      counter=counter+1;
+    }
+  }
 }
+return counter
+}
+
 
 module.exports = {
   countCats
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform( arr ) {
+
+  if (Array.isArray(arr)===false) {
+    throw new Error('\'arr\' parameter must be an instance of the Array!')
+  }
+  let arr2=arr.slice()
+ 
+  let result = arr2.map((et,index)=>{
+    if(et ==='--double-next' && arr2[index+1]) {
+      arr2.splice(index, 1, arr2[index+1])
+    } else if (et === '--double-next' && !arr2[index+1]) {arr2.splice(index, 1)}
+    if(et === '--discard-prev' && arr2[index-1]) {
+      arr2.splice((index-1), 2);
+    } else if (et === '--discard-prev' && !arr2[index-1]) {arr2.splice(index, 1)}
+
+    if(et === '--discard-next' && (arr2[index+2]==='--discard-prev' || arr2[index+2]==='--double-prev')) {
+      arr2.splice((index), 1);
+      
+    }
+
+    if(et === '--discard-next' && arr2[index+1]) {
+      arr2.splice((index), 2);
+    } else if (et === '--discard-next' && !arr2[index+1]) {arr2.splice(index, 1)}
+
+
+    if(et === '--double-prev' && arr2[index-1]) {
+      arr2.splice(index, 1, arr2[index-1]);
+    } else if (et === '--double-prev' && !arr2[index-1]) {arr2.splice(index, 1)}
+  })
+  return arr2
 }
 
 module.exports = {
